7c7c522146bb58028fbf94df88db8cdf19bcebd9
Eugene Haisenka <evheny0@gmail.com>
2025-03-30 22:42:31 +0300

    Implement simple calculator

--- 1/calculator.rb
+++ 1/calculator.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Calculator
+  def initialize(items)
+    @items = items
+  end
+
   def call
+    @items.map do |item|
+      item.price * item.quantity
+    end
   end
 end
--- 1/spec/calculator_spec.rb
+++ 1/spec/calculator_spec.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "../calculator"
+require_relative "../item"
 
-describe "Calculator" do
-  it "somehow works" do
-    calculator = Calculator.new
-    expect(calculator.call).to eq(nil)
+describe Calculator do
+  let(:item1) { Item.new(name: "book", price: 10, quantity: 2) }
+  let(:item2) { Item.new(name: "chocolate", price: 15, quantity: 3) }
+  let(:items) { [item1, item2] }
+
+  it "calculates total prices for each item" do
+    calculator = described_class.new(items)
+    expect(calculator.call).to eq([20, 45])
+  end
+
+  it "handles an empty item list" do
+    calculator = described_class.new([])
+    expect(calculator.call).to eq([])
   end
 end
